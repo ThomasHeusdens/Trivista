2085e3d1a731bb5accfd2e980e4518149bc791a6
ThomasHeusdens <thomas.heusdens@student.ehb.be>
2025-06-03 13:36:13 +0200

    Updated app.congi.js for deployment

--- TrivistaApp/app.config.js
+++ TrivistaApp/app.config.js
@@ -2,18 +2,27 @@ import "dotenv/config";
 
 export default {
   expo: {
-    name: "TrivistaApp",
-    slug: "TrivistaApp",
+    name: "Trivista",
+    slug: "Trivista",
     version: "1.0.0",
+    description: "Trivista helps complete beginners prepare for their first sprint triathlon with an easy 12-week plan — no prior experience needed. From training and nutrition to stretching and recovery, the app guides you every step of the way.",
+    owner: "Thomas Heusdens",
     orientation: "portrait",
-    icon: "./assets/images/icon.png",
+    icon: "./assets/images/logo-trivista.png",
     scheme: "trivistaapp",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
-    backgroundColor: "#1E1E1E",
+    splash: {
+      image: "./assets/images/logo-trivista.png",
+      resizeMode: "contain",
+      backgroundColor: "#1E1E1E",
+    },
     ios: {
+      buildNumber: "1",
       supportsTablet: true,
       bundleIdentifier: "com.trivista.app",
+      googleServicesFile: "./GoogleService-Info.plist",
+      icon: "./assets/images/logo-trivista.png",
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY_IOS,
       },
@@ -24,9 +33,12 @@ export default {
       },
     },
     android: {
+      versionCode: 1,
+      googleServicesFile: "./google-services.json",
+      primaryColor: "#FACC15",
       adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff",
+        foregroundImage: "./assets/images/adaptative-logo.png",
+        backgroundColor: "#FACC15",
       },
       edgeToEdgeEnabled: true,
       package: "com.trivista.app",
@@ -54,10 +66,10 @@ export default {
       [
         "expo-splash-screen",
         {
-          image: "./assets/images/splash-icon.png",
+          image: "./assets/images/logo-trivista.png",
           imageWidth: 200,
           resizeMode: "contain",
-          backgroundColor: "#000000",
+          backgroundColor: "#1E1E1E",
         },
       ],
       "expo-audio",
